b6171a7c450c722a9012f50e062da87a94ba7d50
Eric Adams <eric.adams@myhomepay.com>
2019-06-04 18:12:06 -0500

    serve flava and logger

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ericadams/flav
 
 go 1.12
+
+require (
+	github.com/apex/log v1.1.0
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,52 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/json"
 	"github.com/go-chi/chi"
+	"io"
+	"net/http"
+	"os"
+	"time"
 )
 
+const Port = 3000
+
 func main() {
+
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(":3000", r)
+	r.Get("/flava", ServeFlava)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", Port), r)
+}
+
+func getLogger() *log.Entry {
+	log.SetHandler(json.New(os.Stdout))
+	return log.WithField("service", "flava").
+		WithField("port", Port)
+}
+
+func ServeFlava(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("./img/Flava_Flav.jpg")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, file)
+
+}
+
+func ServerTime(w http.ResponseWriter, r *http.Request) {
+
+	time.Now().UTC()
+
 }
